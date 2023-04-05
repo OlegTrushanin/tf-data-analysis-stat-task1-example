45acea1365f64d1cbb8c805c1eee467eb45bdf47
OlegTrushanin <83016212+OlegTrushanin@users.noreply.github.com>
2023-04-05 20:52:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,9 +5,11 @@ import numpy as np
 chat_id = 767458283 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    b = np.median(np.abs(x - np.median(x))) / 0.6745
-    
-    # Находим точечную оценку коэффициента ускорения
-    a = 2 * x.mean() / 80 ** 2 / (b * np.sqrt(2))
-  
-    return a
+     def log_likelihood(x, t):
+        b = np.median(np.abs(x - np.median(x))) / 0.6745
+        a = 2 * x.mean() / t ** 2 / (b * np.sqrt(2))
+        return np.sum(np.log(np.exp(-np.abs(x - a * t) / b) / (2 * b)))
+
+    t = 80
+    res = minimize_scalar(lambda t: -log_likelihood(x, t), bounds=(0.01, 1000), method='bounded')
+    return 2 * x.mean() / res.x ** 2 / (np.median(np.abs(x - np.median(x))) / 0.6745 * np.sqrt(2))
