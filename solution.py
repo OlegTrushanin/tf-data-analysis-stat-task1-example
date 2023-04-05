d7ee6bffe8d9f4fa2b7f6cf76f1a3df6544a3c4d
OlegTrushanin <83016212+OlegTrushanin@users.noreply.github.com>
2023-04-05 20:22:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import numpy as np
 chat_id = 767458283 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    b = np.median(np.abs(x - x.median())) / 0.6745
+    b = np.median(np.abs(x - np.median(x))) / 0.6745
     
     # Находим точечную оценку коэффициента ускорения
     a = 2 * x.mean() / 80 ** 2 / (b * np.sqrt(2))
