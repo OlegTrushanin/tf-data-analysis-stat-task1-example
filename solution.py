3adf3965372dfcd5addfa462f035c1f5933b835c
OlegTrushanin <83016212+OlegTrushanin@users.noreply.github.com>
2023-04-05 20:38:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,4 +9,10 @@ def solution(x: np.array) -> float:
     
     # Находим точечную оценку коэффициента ускорения
     a = 2 * x.mean() / 80 ** 2 / (b * np.sqrt(2))
+    sample_sizes = [10, 100, 1000]
+    thresholds = [2.14E-8, 6.26E-9, 1.94E-9]
+    for i in range(len(sample_sizes)):
+        sample_size = sample_sizes[i]
+        threshold = thresholds[i]
+        mse = np.mean((x[:sample_size] - a * np.arange(80) ** 2 / 2) ** 2)
     return a
