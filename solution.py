4a1e9c562d594d618ccb84eeeccbc11157680f3f
OlegTrushanin <83016212+OlegTrushanin@users.noreply.github.com>
2023-04-05 20:53:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,10 +5,10 @@ import numpy as np
 chat_id = 767458283 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-     def log_likelihood(x, t):
-        b = np.median(np.abs(x - np.median(x))) / 0.6745
-        a = 2 * x.mean() / t ** 2 / (b * np.sqrt(2))
-        return np.sum(np.log(np.exp(-np.abs(x - a * t) / b) / (2 * b)))
+    def log_likelihood(x, t):
+         b = np.median(np.abs(x - np.median(x))) / 0.6745
+         a = 2 * x.mean() / t ** 2 / (b * np.sqrt(2))
+         return np.sum(np.log(np.exp(-np.abs(x - a * t) / b) / (2 * b)))
 
     t = 80
     res = minimize_scalar(lambda t: -log_likelihood(x, t), bounds=(0.01, 1000), method='bounded')
