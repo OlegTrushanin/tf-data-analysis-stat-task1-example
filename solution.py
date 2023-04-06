62354a07b599cea249f684c7774c14e7b74e9f5c
OlegTrushanin <83016212+OlegTrushanin@users.noreply.github.com>
2023-04-06 20:44:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,13 @@ from scipy.optimize import minimize
 chat_id = 767458283 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    mu = x.mean()
-    b = (np.abs(x - mu)).mean()
-    return 2/b**2
+    time = 80.0  # время в секундах
+    
+    # Средний пройденный путь
+    mean_distance = np.mean(x)
+    
+    
+    mean_speed = mean_distance / time  # средняя скорость
+    acceleration = mean_speed**2 / (2 * mean_distance)  # оценка ускорения
+    
+    return acceleration
